6ff42cd107c7d93013472f3f5e20c9f7fcdd66c2
kaybee-mn <121321741+kaybee-mn@users.noreply.github.com>
2025-07-31 17:48:51 -0700

    feat: include timezone in signup request and user creation

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -7,23 +7,23 @@ const JWT_SECRET = process.env.JWT_SECRET!;
 const authRoutes = async (fastify: FastifyInstance) => {
   // Signup
   fastify.post("/signup", async (request, reply) => {
-    const { email, password } = request.body as {
+    const { email, password,timezone } = request.body as {
       email: string;
       password: string;
+      timezone:string;
     };
 
     const { data, error } = await supabase.auth.signUp({ email, password });
     if (error) {
       return reply.code(400).send({ error: error.message });
     }
-    const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
     // Create user in your DB (optional)
     await fastify.prisma.user.create({
       data: {
         email,
         supabaseId: data.user?.id || "",
-        timezone:tz
+        timezone:timezone
       },
     });
 
